f62e609d2ad6279f219ee89f59698a554a601709
Will Sturgeon <willstrgn@gmail.com>
2023-08-16 13:24:32 -0700

    Enable all features while testing in CI

--- ci.sh
+++ ci.sh
@@ -8,7 +8,7 @@ rustup component add miri --toolchain nightly
 cargo fmt --check
 cargo clippy --all-targets --no-default-features
 cargo clippy --all-targets --all-features
-RUST_BACKTRACE=1 cargo test -r --features=quickcheck
+RUST_BACKTRACE=1 cargo test -r --all-features
 MIRIFLAGS=-Zmiri-backtrace=full cargo +nightly miri test --target=x86_64-apple-darwin
 MIRIFLAGS=-Zmiri-backtrace=full cargo +nightly miri test --target=x86_64-unknown-linux-gnu
 MIRIFLAGS=-Zmiri-backtrace=full cargo +nightly miri test --target=aarch64-apple-darwin
